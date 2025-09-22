1d69683206668a2b9f58bdeb3b5a400c988ac733
Glaucio Campos <glaucio.campos@hotmail.com>
2025-09-22 00:22:38 -0300

    fix(subnets): attach public security list to public subnet

--- modules/oci-network/main.tf
+++ modules/oci-network/main.tf
@@ -1,5 +1,3 @@
-# modules/oci-network/main.tf
-
 terraform {
   required_version = ">= 1.13.0"
   required_providers {
@@ -36,23 +34,6 @@ resource "oci_core_route_table" "public_route_table" {
   }
 }
 
-resource "oci_core_subnet" "public_subnet" {
-  count             = var.create_public_subnet ? 1 : 0
-  compartment_id    = var.compartment_id
-  vcn_id            = oci_core_vcn.vcn.id
-  cidr_block        = var.public_subnet_cidr
-  display_name      = "${var.prefix}-public-subnet"
-  route_table_id    = oci_core_route_table.public_route_table[0].id
-  security_list_ids = [oci_core_security_list.security_list[0].id]
-}
-
-resource "oci_core_nat_gateway" "nat_gateway" {
-  count          = var.create_private_subnet ? 1 : 0
-  compartment_id = var.compartment_id
-  vcn_id         = oci_core_vcn.vcn.id
-  display_name   = "${var.prefix}-nat"
-}
-
 resource "oci_core_route_table" "private_route_table" {
   count          = var.create_private_subnet ? 1 : 0
   compartment_id = var.compartment_id
@@ -64,19 +45,15 @@ resource "oci_core_route_table" "private_route_table" {
   }
 }
 
-resource "oci_core_subnet" "private_subnet" {
-  count                      = var.create_private_subnet ? 1 : 0
-  compartment_id             = var.compartment_id
-  vcn_id                     = oci_core_vcn.vcn.id
-  cidr_block                 = var.private_subnet_cidr
-  display_name               = "${var.prefix}-private-subnet"
-  route_table_id             = oci_core_route_table.private_route_table[0].id
-  security_list_ids          = [oci_core_security_list.security_list[0].id]
-  prohibit_public_ip_on_vnic = true
+resource "oci_core_nat_gateway" "nat_gateway" {
+  count          = var.create_private_subnet ? 1 : 0
+  compartment_id = var.compartment_id
+  vcn_id         = oci_core_vcn.vcn.id
+  display_name   = "${var.prefix}-nat"
 }
 
-resource "oci_core_security_list" "security_list" {
-  count          = var.create_public_subnet || var.create_private_subnet ? 1 : 0
+resource "oci_core_security_list" "private_security_list" {
+  count          = var.create_private_subnet ? 1 : 0
   compartment_id = var.compartment_id
   vcn_id         = oci_core_vcn.vcn.id
   display_name   = "${var.prefix}-private-security-list"
@@ -88,8 +65,8 @@ resource "oci_core_security_list" "security_list" {
   }
 
   ingress_security_rules {
-    protocol    = "6"
-    source      = var.vcn_cidr
+    protocol = "6"
+    source   = var.vcn_cidr
     description = "Allow OCFS2 heartbeat traffic between instances"
     tcp_options {
       min = 7777
@@ -125,8 +102,8 @@ resource "oci_core_security_list" "security_list" {
   }
 
   ingress_security_rules {
-    protocol    = "6"
-    source      = "0.0.0.0/0"
+    protocol = "6"
+    source   = "0.0.0.0/0"
     description = "Allow HTTP traffic"
     tcp_options {
       min = 80
@@ -135,12 +112,63 @@ resource "oci_core_security_list" "security_list" {
   }
 
   ingress_security_rules {
-    protocol    = "6"
-    source      = "0.0.0.0/0"
+    protocol = "6"
+    source   = "0.0.0.0/0"
     description = "Allow HTTPS traffic"
     tcp_options {
       min = 443
       max = 443
     }
   }
+}
+
+resource "oci_core_security_list" "public_security_list" {
+  count          = var.create_public_subnet ? 1 : 0
+  compartment_id = var.compartment_id
+  vcn_id         = oci_core_vcn.vcn.id
+  display_name   = "${var.prefix}-public-security-list"
+
+  egress_security_rules {
+    destination = "0.0.0.0/0"
+    protocol    = "all"
+  }
+
+  ingress_security_rules {
+    protocol = "6"
+    source   = "0.0.0.0/0"
+    tcp_options {
+      min = 80
+      max = 80
+    }
+  }
+
+  ingress_security_rules {
+    protocol = "6"
+    source   = "0.0.0.0/0"
+    tcp_options {
+      min = 443
+      max = 443
+    }
+  }
+}
+
+resource "oci_core_subnet" "private_subnet" {
+  count                      = var.create_private_subnet ? 1 : 0
+  compartment_id             = var.compartment_id
+  vcn_id                     = oci_core_vcn.vcn.id
+  cidr_block                 = var.private_subnet_cidr
+  display_name               = "${var.prefix}-private-subnet"
+  route_table_id             = oci_core_route_table.private_route_table[0].id
+  security_list_ids          = [oci_core_security_list.private_security_list[0].id]
+  prohibit_public_ip_on_vnic = true
+}
+
+resource "oci_core_subnet" "public_subnet" {
+  count             = var.create_public_subnet ? 1 : 0
+  compartment_id    = var.compartment_id
+  vcn_id            = oci_core_vcn.vcn.id
+  cidr_block        = var.public_subnet_cidr
+  display_name      = "${var.prefix}-public-subnet"
+  route_table_id    = oci_core_route_table.public_route_table[0].id
+  security_list_ids = [oci_core_security_list.public_security_list[0].id]
 }
